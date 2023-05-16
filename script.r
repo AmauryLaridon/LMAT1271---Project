48cfe2475238522fae1fa83250ab83a5735a4df4
Amaury Laridon <amaurylaridon@protonmail.com>
2023-05-16 18:23:43 +0200

    cleaning fig

--- script.r
+++ script.r
@@ -1,10 +1,24 @@
-n <- 20
-nbr_replication <- 100
-a_0 <- 0.5
-b_0 <- 1.2
+################################################################################
+#                                                                              #
+#                              LMAT 1271                                       #
+#                           Projet 2022-2023                                   #
+#         Auteurs : Marie Determe, Augustin Lambotte, Amaury Laridon           #
+#         Script R utiliser pour le projet instructions et ressources          #
+#       disponibles à : https://github.com/AmauryLaridon/LMAT1271-Project      #
+#                                                                              #
+################################################################################
+
+rm(list=ls(all=TRUE)) # Nettoyage mémoire R Studio
 set.seed(2023)
 
-#Fonction de densit?
+######################### - Partie 1.3 Simulations - ###########################
+
+n <- 20 # size of the sampling
+nbr_replication <- 100 # number of replication of the sampling process
+a_0 <- 0.5 # alpha parameter of the density function of T
+b_0 <- 1.2 # beta parameter of the density function of T
+
+#Density function of random variable T
 f_a_0b_0 <- function(t) {
   if(t > 0 & t < 1) {
     return(a_0*b_0*t^(a_0-1)*(1-t^a_0)^(b_0-1))
@@ -32,7 +46,7 @@ q_s <- function(T_i) {
 
 #Estimator of a_m and b_m, using the method of moments
 q_m <- function(T_i) {
-  #On cherche une racine ? la premi?re ?quation de 1.2.b pour obtenir a_M, puis on calcule b_M et q_M
+  #On cherche une racine à la première équation de 1.2.b pour obtenir a_M, puis on calcule b_M et q_M
   f1 <- function(a) {
     return ((sum(T_i^(2*a)))*(n+sum(T_i^(a)))*(sum(T_i^(a)))^(-2)-2)
   }
@@ -44,9 +58,9 @@ q_m <- function(T_i) {
 
 #Estimator of a_l and b_l, using the maximum likelihood estimator
 q_l <- function(T_i) {
-  #On cherche une racine ? la premi?re ?quation de 1.2.c pour obtenir a_L, puis on calcule b_L et q_L
+  #On cherche une racine à la première équation de 1.2.c pour obtenir a_L, puis on calcule b_L et q_L
   f1 <- function(a) {
-    return (1/a + (1/(sum(log(1-T_i)))+1/n)*sum(T_i^(a)*log(T_i)/(1-T_i^(a)))+1/n*sum(log(T_i)))
+    return (1/a + (1/(sum(log(1-T_i^a)))+(1/n))*sum((T_i^(a)*log(T_i))/(1-T_i^(a)))+(1/n)*sum(log(T_i)))
   }
   solution_l <- uniroot(f1,c(1e-9,10))
   a_l <- solution_l$root
@@ -54,6 +68,8 @@ q_l <- function(T_i) {
   
   return (q_p_a_b(0.95,a_l,b_l))
 }
+
+### - 1.3 (a) - ###
 
 #Generation of a iid sample of size 20 from the density f_a_0b_0 and estimation of q_0.95 in 3 different ways
 generate_sample <- function() {
@@ -65,6 +81,8 @@ generate_sample <- function() {
   return (c(q_s_sample,q_m_sample,q_l_sample))
 }
 
+### - 1.3 (b) - ###
+
 generate_estimation_q_p <- function(affichage) {
   estimation_q_p <- replicate(nbr_replication,generate_sample())
   estimation_q_s <- estimation_q_p[1,]
@@ -72,8 +90,9 @@ generate_estimation_q_p <- function(affichage) {
   estimation_q_l <- estimation_q_p[3,]
   
   
+### - 1.3 (c) - ###
   
-  #Approximation de l'esp?rance par la moyenne empirique
+  #Approximation de l'espérance par la moyenne empirique
   esperance_q_s <- mean(estimation_q_s)
   esperance_q_m <- mean(estimation_q_m)
   esperance_q_l <- mean(estimation_q_l)
@@ -95,26 +114,28 @@ generate_estimation_q_p <- function(affichage) {
 
   if (affichage == TRUE) {
     hist(estimation_q_s,breaks=10, main="")
-    title(main = paste("n =", n, " N =", nbr_replication, " a_0 =", a_0, " b_0 =", b_0), sub= paste("Biais =", round(bias_s, digits=4)  , " Variance=", round(variance_s, digits=4), " MSE=", round(mse_s, digits=4)))
-    boxplot(estimation_q_s, xlab = "q_s", main = paste("n =", n, " N =", nbr_replication, " a_0 =", a_0, " b_0 =", b_0))
-    title(main = paste("n =", n, " N =", nbr_replication, " a_0 =", a_0, " b_0 =", b_0), sub= paste("Biais =", round(bias_s, digits=4)  , " Variance=", round(variance_s, digits=4), " MSE=", round(mse_s, digits=4)))
+    title(main = paste("Histogram of estimated q_s\n", "n =", n, " N =", nbr_replication, " a_0 =", a_0, " b_0 =", b_0), sub= paste("Biais =", round(bias_s, digits=4)  , " Variance=", round(variance_s, digits=4), " MSE=", round(mse_s, digits=4)))
+    boxplot(estimation_q_s, xlab = "q_s", ylab="Value")
+    title(main = paste("Boxplot of estimated q_s\n","n =", n, " N =", nbr_replication, " a_0 =", a_0, " b_0 =", b_0), sub= paste("Biais =", round(bias_s, digits=4)  , " Variance=", round(variance_s, digits=4), " MSE=", round(mse_s, digits=4)))
     
     
     hist(estimation_q_m,breaks=10, main = "")
-    title(main = paste("n =", n, " N =", nbr_replication, " a_0 =", a_0, " b_0 =", b_0), sub= paste("Biais =", round(bias_m, digits=4)  , " Variance=", round(variance_m, digits=4), " MSE=", round(mse_m, digits=4)))
-    boxplot(estimation_q_m, xlab = "q_m", main = "")
-    title(main = paste("n =", n, " N =", nbr_replication, " a_0 =", a_0, " b_0 =", b_0), sub= paste("Biais =", round(bias_m, digits=4)  , " Variance=", round(variance_m, digits=4), " MSE=", round(mse_m, digits=4)))
+    title(main = paste("Histogram of estimated q_m\n","n =", n, " N =", nbr_replication, " a_0 =", a_0, " b_0 =", b_0), sub= paste("Biais =", round(bias_m, digits=4)  , " Variance=", round(variance_m, digits=4), " MSE=", round(mse_m, digits=4)))
+    boxplot(estimation_q_m, xlab = "q_m", main = "", ylab="Value")
+    title(main = paste("Boxplot of estimated q_m\n","n =", n, " N =", nbr_replication, " a_0 =", a_0, " b_0 =", b_0), sub= paste("Biais =", round(bias_m, digits=4)  , " Variance=", round(variance_m, digits=4), " MSE=", round(mse_m, digits=4)))
     
     
     hist(estimation_q_l,breaks=10, main = "")
-    title(main = paste("n =", n, " N =", nbr_replication, " a_0 =", a_0, " b_0 =", b_0), sub= paste("Biais =", round(bias_l, digits=4)  , " Variance=", round(variance_l, digits=4), " MSE=", round(mse_l, digits=4)))
-    boxplot(estimation_q_l, xlab = "q_l" , main = "")   
-    title(main = paste("n =", n, " N =", nbr_replication, " a_0 =", a_0, " b_0 =", b_0), sub= paste("Biais =", round(bias_l, digits=4)  , " Variance=", round(variance_l, digits=4), " MSE=", round(mse_l, digits=4)))
+    title(main = paste("Histogram of estimated q_l\n","n =", n, " N =", nbr_replication, " a_0 =", a_0, " b_0 =", b_0), sub= paste("Biais =", round(bias_l, digits=4)  , " Variance=", round(variance_l, digits=4), " MSE=", round(mse_l, digits=4)))
+    boxplot(estimation_q_l, xlab = "q_l" , main = "", ylab="Value")   
+    title(main = paste("Boxplot of estimated q_l\n","n =", n, " N =", nbr_replication, " a_0 =", a_0, " b_0 =", b_0), sub= paste("Biais =", round(bias_l, digits=4)  , " Variance=", round(variance_l, digits=4), " MSE=", round(mse_l, digits=4)))
     
   }
   
   return (c(c(bias_s,bias_m,bias_l),c(variance_s,variance_m,variance_l),c(mse_s,mse_m,mse_l)))
 }
+
+#################### - Execution - ######################
 
 #Vraie valeur de q_0.95
 q_p_true <- q_p(0.95)
