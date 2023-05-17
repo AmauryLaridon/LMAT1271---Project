ee55bd71f8786b805c9ef9a202922258e9730afd
Amaury Laridon <amaurylaridon@protonmail.com>
2023-05-17 11:29:48 +0200

    update (a)

---
+++ regression.r
@@ -0,0 +1,36 @@
+################################################################################
+#                                                                              #
+#                              LMAT 1271                                       #
+#                           Projet 2022-2023                                   #
+#         Auteurs : Marie Determe, Augustin Lambotte, Amaury Laridon           #
+#         Script R utiliser pour le projet instructions et ressources          #
+#       disponibles à : https://github.com/AmauryLaridon/LMAT1271-Project      #
+#                                                                              #
+################################################################################
+
+rm(list=ls(all=TRUE)) # Nettoyage mémoire R Studio
+set.seed(2023)
+
+######################### - Partie 2 Regression - ###########################
+
+### Importation des données ### 
+
+dataproject <- read.csv("~/Bureau/LMAT1271 - Calcul des probabilités et analyse statistique/Projet/Data/dataproject.txt", sep=";")
+#View(dataproject)
+
+### Question (a) ###
+
+lm1 <- lm(Y~X, data = dataproject) #Calcul et création d'un modèle de regression
+                                   #linéaire
+
+
+
+
+
+
+
+
+
+
+
+
